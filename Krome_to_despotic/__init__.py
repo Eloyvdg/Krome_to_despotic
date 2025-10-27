a91cd959e676aa1a168794b3d9b249d6e703045a
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-10-27 14:50:34 +0100

    Update __init__.py

--- Krome_to_despotic/__init__.py
+++ Krome_to_despotic/__init__.py
@@ -1,3 +1,13 @@
+'''
+  ! *************************************************************
+  !  Krome_to_despotic
+  !
+  !  Written by Eloy van de Genugten (Leiden University, 2025)
+  !  With Piyush Sharda (LU), Jackie Hodge (LU) and Shyam Menon (CCA)
+  !  Email: eloyvandegenugten@gmail.com, sharda@strw.leidenuniv.nl
+  !  Krome_to_despotic is provided "as it is", without any warranty.
+  ! *************************************************************
+'''
 
 __version__ = "0.0.1"
 __all__ = ["Pipeline", "utils"]
