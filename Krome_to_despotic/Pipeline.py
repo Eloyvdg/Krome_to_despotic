ff4ad434ebeda254e2762c9a2f3c2ab060a40798
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-10-27 14:50:24 +0100

    Update Pipeline.py

--- Krome_to_despotic/Pipeline.py
+++ Krome_to_despotic/Pipeline.py
@@ -1,3 +1,14 @@
+'''
+  ! *************************************************************
+  !  Krome_to_despotic
+  !
+  !  Written by Eloy van de Genugten (Leiden University, 2025)
+  !  With Piyush Sharda (LU), Jackie Hodge (LU) and Shyam Menon (CCA)
+  !  Email: eloyvandegenugten@gmail.com, sharda@strw.leidenuniv.nl
+  !  Krome_to_despotic is provided "as it is", without any warranty.
+  ! *************************************************************
+'''
+
 from tabnanny import verbose
 import numpy as np
 import os 
